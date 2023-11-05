eb5fcb2ccda00c77e0d7238b32ef26e28e6db42b
Bauer Heiko <bha5si@bosch.com>
2023-11-05 11:19:05 +0100

    - Ignoring outlier values when posting to openhab

--- smart_meter_to_openhab/sml_iskra_mt175.py
+++ smart_meter_to_openhab/sml_iskra_mt175.py
@@ -89,12 +89,13 @@ class SmlReader():
     def read_and_validate_from_sml(self, oh_connection : OpenhabConnection, time_out : timedelta = timedelta(seconds=20)) -> SmartMeterValues:
         def has_outlier(value_list : List[Any], ref_value_list : List[Any]) -> bool:
             for i in range(len(value_list)):
-                if ref_value_list[i] is not None and value_list[i]*0.05 > ref_value_list[i]:
+                if ref_value_list[i] is not None and value_list[i]*0.01 > ref_value_list[i]:
                     return True
             return False
         ref_value_list = oh_connection.get_median_from_items(SmartMeterValues.oh_item_names).convert_to_value_list()
         time_start=datetime.now()
         values=SmartMeterValues()
+        value_list=values.convert_to_value_list()
         while (datetime.now() - time_start) <= time_out:
             current_values=self.read_from_sml()
             if current_values.has_none_value():
@@ -106,9 +107,11 @@ class SmlReader():
                 self._logger.info(f"Detected unrealistic values during SML read. Trying again")
                 continue
             break
-
-        if (datetime.now() - time_start) > time_out:
-            # log a warning message but return the latest none value. It might be correct though it is an outlier.
-            self._logger.warning(f"Exceeded time out of {time_out} while validating values from smart meter.")
+        
+        if values.has_none_value() or has_outlier(value_list, ref_value_list):
+            self._logger.warning(f"Unable to read and validate SML data. Ignoring following values: "\
+                f"L1={values.phase_1_consumption.value} L2={values.phase_2_consumption.value} "\
+                f"L3={values.phase_3_consumption.value} Overall={values.overall_consumption.value} E={values.electricity_meter.value}")
+            values.reset()
 
         return values
